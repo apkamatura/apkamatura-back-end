b1c71871f0c0a6f93cb1b6a57ffec622f1e51966
Michał <olszewskimichal@outlook.com>
2018-03-01 20:08:38 +0100

    inicjalizacja projektu - Spring Boot w wersji 2.0 ale uzywajacy javy 8

---
+++ src/test/java/pl/apka/matura/apkamatura/ApkamaturaApplicationTests.java
@@ -0,0 +1,16 @@
+package pl.apka.matura.apkamatura;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ApkamaturaApplicationTests {
+
+  @Test
+  public void contextLoads() {
+  }
+
+}
